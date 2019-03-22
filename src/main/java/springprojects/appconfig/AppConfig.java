cb7c10f601585086d1b27e5b5908e392ca9c4085
Oksana999 <oksanam999@gmail.com>
2019-03-22 17:03:29 -0500

    starting project

---
+++ src/main/java/springprojects/appconfig/AppConfig.java
@@ -0,0 +1,9 @@
+package springprojects.appconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("springprojects")
+public class AppConfig {
+}
